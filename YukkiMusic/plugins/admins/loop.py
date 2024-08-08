8b19e4c97380aab6f75f692291fe2e6de1964107
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-08-08 16:38:29 +0300

    Update loop.py

--- YukkiMusic/plugins/admins/loop.py
+++ YukkiMusic/plugins/admins/loop.py
@@ -1,27 +1,26 @@
 #
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 #
 
 from pyrogram import filters
 from pyrogram.types import Message
-
 from config import BANNED_USERS
-from strings import get_command
+from strings.filters import command
 from YukkiMusic import app
 from YukkiMusic.utils.database.memorydatabase import get_loop, set_loop
 from YukkiMusic.utils.decorators import AdminRightsCheck
 
 # Commands
-LOOP_COMMAND = get_command("LOOP_COMMAND")
+@app.on_message(
+    command(["تكرار","كرر","انسخ"])
+)
 
-
-@app.on_message(filters.command(LOOP_COMMAND) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def admins(cli, message: Message, _, chat_id):
     usage = _["admin_24"]
@@ -42,12 +41,12 @@ async def admins(cli, message: Message, _, chat_id):
             )
         else:
             return await message.reply_text(_["admin_26"])
-    elif state.lower() == "enable":
+    elif state.lower() == "تشغيل":
         await set_loop(chat_id, 10)
         return await message.reply_text(
             _["admin_25"].format(message.from_user.first_name, 10)
         )
-    elif state.lower() == "disable":
+    elif state.lower() == "ايقاف":
         await set_loop(chat_id, 0)
         return await message.reply_text(_["admin_27"])
     else:
