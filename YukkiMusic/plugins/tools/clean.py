41fb45aeb7a0ffc28a71e21963e95f2d08f350c6
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-08-08 17:19:03 +0300

    Update clean.py

--- YukkiMusic/plugins/tools/clean.py
+++ YukkiMusic/plugins/tools/clean.py
@@ -1,19 +1,24 @@
 import os
 import shutil
-
 from pyrogram import filters
-
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from strings.filters import command
 from YukkiMusic import app
 from YukkiMusic.misc import SUDOERS
+from config import SUPPORT_CHANNEL
 
-
-@app.on_message(filters.command("clean") & SUDOERS)
+@app.on_message(command(["تنظيف السجلات","⦗ تنظيف السجلات ⦘"]) & SUDOERS)
 async def clean(_, message):
-    A = await message.reply_text("ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...")
+    A = await message.reply_text("- جاري التنظيف الأن .")
     dir = "downloads"
     dir1 = "cache"
     shutil.rmtree(dir)
     shutil.rmtree(dir1)
     os.mkdir(dir)
     os.mkdir(dir1)
-    await A.edit("ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs ᴀʀᴇ ᴄʟᴇᴀɴᴇᴅ")
+    
+    keyboard = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("⦗ قناة التحديثات ⦘", url=SUPPORT_CHANNEL)]]
+    )
+    
+    await A.edit("- تم التنظيف بنجاح .", reply_markup=keyboard)
