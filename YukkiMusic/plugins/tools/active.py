f7fdcce1fa7a28c1e3959dbcb6923c4425889937
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-08-08 17:18:34 +0300

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -1,96 +1,29 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 from pyrogram import filters
-from pyrogram.types import Message
-
-from strings import get_command
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from YukkiMusic import app
 from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database.memorydatabase import (
-    get_active_chats,
-    get_active_video_chats,
-)
+from YukkiMusic.utils.database.memorydatabase import get_active_chats, get_active_video_chats
+from config import SUPPORT_CHANNEL
+from strings.filters import command
 
-# Commands
-ACTIVEVC_COMMAND = get_command("ACTIVEVC_COMMAND")
-ACTIVEVIDEO_COMMAND = get_command("ACTIVEVIDEO_COMMAND")
-
-
-@app.on_message(filters.command(ACTIVEVC_COMMAND) & SUDOERS)
-async def activevc(_, message: Message):
-    mystic = await message.reply_text("ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ....ᴘʟᴇᴀsᴇ ʜᴏʟᴅ ᴏɴ")
-    served_chats = await get_active_chats()
-    text = ""
-    j = 0
-    for x in served_chats:
-        try:
-            title = (await app.get_chat(x)).title
-        except Exception:
-            title = "ᴘʀɪᴠᴀᴛᴇ ɢʀᴏᴜᴘ"
-        if (await app.get_chat(x)).username:
-            user = (await app.get_chat(x)).username
-            text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})[`{x}`]\n"
-        else:
-            text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
-        j += 1
-    if not text:
-        await mystic.edit_text("ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ's")
-    else:
-        await mystic.edit_text(
-            f"**ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ's:-**\n\n{text}",
-            disable_web_page_preview=True,
+@app.on_message(command(["⦗ الاتصالات النشطة ⦘", "الاتصالات النشطة"]))
+async def active_chats(_, message: Message):
+    if message.from_user.id not in SUDOERS:
+        return await message.reply_text(
+            "عذرًا، هذا الأمر مخصص للمطور فقط -"
         )
 
-
-@app.on_message(filters.command(ACTIVEVIDEO_COMMAND) & SUDOERS)
-async def activevi_(_, message: Message):
-    mystic = await message.reply_text("ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ....ᴘʟᴇᴀsᴇ ʜᴏʟᴅ ᴏɴ")
-    served_chats = await get_active_video_chats()
-    text = ""
-    j = 0
-    for x in served_chats:
-        try:
-            title = (await app.get_chat(x)).title
-        except Exception:
-            title = "ᴘʀɪᴠᴀᴛᴇ ɢʀᴏᴜᴘ"
-        if (await app.get_chat(x)).username:
-            user = (await app.get_chat(x)).username
-            text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})[`{x}`]\n"
-        else:
-            text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
-        j += 1
-    if not text:
-        await mystic.edit_text("ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛ's")
-    else:
-        await mystic.edit_text(
-            f"**ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛ's:-**\n\n{text}",
-            disable_web_page_preview=True,
-        )
-
-
-@app.on_message(filters.command(["ac"]) & SUDOERS)
-async def vc(client, message: Message):
     ac_audio = str(len(await get_active_chats()))
     ac_video = str(len(await get_active_video_chats()))
-    await message.reply_text(
-        f"✫ <b><u>ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ɪɴғᴏ</u></b> :\n\nᴠᴏɪᴄᴇ : {ac_audio}\nᴠɪᴅᴇᴏ  : {ac_video}"
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton("⦗ قناة التحديثات ⦘", url=SUPPORT_CHANNEL)],
+        ]
     )
 
-
-__MODULE__ = "Acᴛɪᴠᴇ"
-__HELP__ = """
-<b>✧ /ac</b> - Cʜᴇᴄᴋ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴏɴ ʙᴏᴛ.
-
-<b>✧ /activevoice</b> - Cʜᴇᴄᴋ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴀɴᴅ ᴠɪᴅᴇᴏ ᴄᴀʟʟs ᴏɴ ʙᴏᴛ.
-
-<b>✧ /activevideo</b> - Cʜᴇᴄᴋ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄᴀʟʟs ᴏɴ ʙᴏᴛ.
-
-<b>✧ /stats</b> - Cʜᴇᴄᴋ Bᴏᴛs Sᴛᴀᴛs
-"""
+    # تعديل التنسيق ليكون على سطر واحد بالكامل بدون فواصل غير ضرورية
+    await message.reply_text(
+        f"-› عزيزي المطور هذه هي المجموعات الصوتية والفيديو التي يتم تشغيل المساعد فيها:\n\n-› الصوت: {ac_audio}\n-› الفيديو: {ac_video}",
+        reply_markup=keyboard,
+    )
