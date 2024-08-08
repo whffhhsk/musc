a35641fc6e4c9ac63d501f9d8115501f150d49a7
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-08-08 16:41:19 +0300

    Update shuffle.py

--- YukkiMusic/plugins/admins/shuffle.py
+++ YukkiMusic/plugins/admins/shuffle.py
@@ -1,29 +1,27 @@
 #
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 #
 
 import random
-
 from pyrogram import filters
 from pyrogram.types import Message
-
 from config import BANNED_USERS
-from strings import get_command
+from strings.filters import command
 from YukkiMusic import app
 from YukkiMusic.misc import db
 from YukkiMusic.utils.decorators import AdminRightsCheck
 
 # Commands
-SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
+@app.on_message(
+    command(["دمج","عشوائي","مزج"])
+)
 
-
-@app.on_message(filters.command(SHUFFLE_COMMAND) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def admins(Client, message: Message, _, chat_id):
     if not len(message.command) == 1:
