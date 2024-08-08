dded44b151fd800b7aac116f707665248a19f5b8
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-08-08 16:58:01 +0300

    Update playmode.py

--- YukkiMusic/plugins/play/playmode.py
+++ YukkiMusic/plugins/play/playmode.py
@@ -1,32 +1,29 @@
 #
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 #
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
-
+from strings.filters import command
 from config import BANNED_USERS
-from strings import get_command
 from YukkiMusic import app
 from YukkiMusic.utils.database import get_playmode, get_playtype, is_nonadmin_chat
 from YukkiMusic.utils.decorators import language
 from YukkiMusic.utils.inline.settings import playmode_users_markup
 
 ### Commands
-PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
-
-
-@app.on_message(filters.command(PLAYMODE_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    command(["مود التشغيل"]) & ~BANNED_USERS)
 @language
 async def playmode_(client, message: Message, _):
     playmode = await get_playmode(message.chat.id)
-    if playmode == "Direct":
+    if playmode == "المشرفين فقط":
         Direct = True
     else:
         Direct = None
@@ -36,7 +33,7 @@ async def playmode_(client, message: Message, _):
     else:
         Group = None
     playty = await get_playtype(message.chat.id)
-    if playty == "Everyone":
+    if playty == "الجميع":
         Playtype = None
     else:
         Playtype = True
